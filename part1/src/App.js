c8c1229f5058a709c252bffff5652ffeba6600cd
paolo-arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-05-22 16:05:50 +0800

    complete part 1c

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,32 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import {useState} from 'react'
+
+const Display = ({counter}) => <div> {counter} </div>
+const Button = ({handleClick, text}) => <button onClick={handleClick}> {text} </button>
+
+const App = () => {
+  const [counter,setCounter] = useState(0)
+  console.log("Rendering with counter value", counter)
+  const plusOne = () => {
+    console.log("Increase by 1; Value before: ", counter)
+    setCounter(counter+1)
+  }
+  const minusOne = () => {
+    console.log("Decrese by 1; Value before: ", counter)
+    setCounter(counter-1)
+  }
+  const reset = () => {
+    console.log("Reset to 0; Value before: ", counter)
+    setCounter(0)
+  }
+
+  return (
+    <>
+      <Display counter={counter} />
+      <Button handleClick={plusOne} text="+"/>
+      <Button handleClick={minusOne} text="-"/>
+      <Button handleClick={reset} text="Reset"/>
+    </>
+  )
+}
 
 export default App
