e1d51ad28c764498f8088a30bca6dd531ad5de6a
Paolo Arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-05-29 02:26:28 +0800

    Complete Exercise 1.6: unicafe step1

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Heading = ({text}) => (<h1> {text} </h1>)
+const Button = ({handler, text}) => (<button onClick={handler}> {text} </button>)
+const Statistic = ({rating, label}) => (<p> {label}: {rating} </p>)
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    const newGood = good+1
+    setGood(newGood)
+  }
+  const handleNeutral = () => {
+    const newNeutral = neutral+1
+    setNeutral(newNeutral)
+  }
+  const handleBad = () => {
+    const newBad = bad+1
+    setBad(newBad)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading text="Give Feedback" />
+      <Button handler={handleGood} text="Good" />
+      <Button handler={handleNeutral} text="Neutral" />
+      <Button handler={handleBad} text="Bad" />
+      <Heading text="Statistics" />
+      <Statistic label="Good" rating={good} />
+      <Statistic label="Neutral" rating={neutral} />
+      <Statistic label="Bad" rating={bad} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
