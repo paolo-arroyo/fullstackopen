812c4a1b0ef049b7082c4e106d7dd97cc859e855
Paolo Arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-05-29 02:07:19 +0800

    create unicafe React app

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,56 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import { useState } from "react"
+
+const History = (props) => {
+  if (props.data.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons.
+      </div>
+    )
+  }
+  return (
+    <div>
+      History: {props.data.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}> {text} </button>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    let updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    let updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text="Left" />
+      <Button handleClick={handleRightClick} text="Right" />
+      {right}
+      <History data={allClicks}/>
+      <p>Total: {total}</p>
+    </div>
+  )
+}
 
 export default App
