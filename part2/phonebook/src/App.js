30cedd77d1da259e03f28414ae31a1680993b0cc
Paolo Arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-05-30 03:34:21 +0800

    Complete Exercise 2.9 The Phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
 const Phonebook = ({contacts}) => contacts.map(contact => <div key={contact.name}> {contact.name} {contact.number} </div>)
-
 const App = () => {
   const [persons, setPersons] = useState([{ name: '', number: '' }]) 
   const [newName, setNewName] = useState('Input Name')
   const [newNumber, setNewNumber] = useState('Input Number')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
@@ -22,9 +22,16 @@ const App = () => {
   const handleNewNumber = (e) => {
     setNewNumber(e.target.value)
   }
+  const handleFilter = (e) => {
+    setFilter(e.target.value)
+  }
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
   return (
     <div>
       <h2>Phonebook</h2>
+      <input value={filter} onChange={handleFilter} />
+      <h2>Add New </h2>
       <form>
         <div>
           Name: <input value={newName} onChange={handleNewName}/>
@@ -37,7 +44,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Phonebook contacts={persons} />
+      <Phonebook contacts={personsToShow} />
     </div>
   )
 }
