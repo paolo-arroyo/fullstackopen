2a24c412f747a38e6381f88bc3bb0c568d6b1ad9
paolo-arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-05-29 22:35:23 +0800

    Complete Exercise 2.1: Course information step6

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,16 +1,12 @@
-const App = (props) => {
-  const { notes } = props
+import Note from './components/Note'
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
-    </div>
-  )
-}
+const App = ({notes}) => (
+  <div>
+    <h1>Notes</h1>
+    <ul>
+      {notes.map(note => <Note key={note.id} note={note} />)}
+    </ul>
+  </div>
+)
 
 export default App
