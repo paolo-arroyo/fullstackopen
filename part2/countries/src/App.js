46316f6b8cecd4d8e9f4b32d2fb7e6ce1c71025c
Paolo Arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-06-01 02:52:32 +0800

    Complete Exercise 2.20: Data for countries, step3
    API key stored in .env file not included in commit. Still refactoring but exercise objectives have been achieved.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -18,8 +18,22 @@ const Result = ({countries, setSelected, selected}) => {
 }
 
 const Country = ({selected}) => {
+  const [temp, setTemp] = useState('')
+  const [icon, setIcon] = useState('')
+  const [windSpeed, setWindSpeed] = useState('')
+  const api_key = process.env.REACT_APP_API_KEY
+  const weatherapi = `http://api.openweathermap.org/data/2.5/weather?id=524901&appid=${api_key}&units=metric&q=${selected.capital}`
+  
+  useEffect(() => {
+    axios.get(weatherapi).then(response => {
+      const current = response.data
+      setTemp(current.main.temp)
+      setWindSpeed(current.wind.speed)
+      setIcon(current.weather[0].icon)
+    })
+  },[weatherapi])
+
   if (selected !== null) {
-    console.log(selected)
     return (
       <div>
         <h1> {selected.name.common} </h1>
@@ -31,6 +45,10 @@ const Country = ({selected}) => {
           {Object.values(selected.languages).map(l => <li key={l}>{l}</li>)}
         </ul>
         <img src={selected.flags.png} alt={`Flag for ${selected.name.common}`} />
+        <h2> Weather in {selected.capital}</h2>
+        <p>Temperature: {temp} Celcius </p>
+        <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt={`Weather in ${selected.capital}`} />
+        <p> Wind: {windSpeed} m/s </p>
       </div>
     )
   } else {
