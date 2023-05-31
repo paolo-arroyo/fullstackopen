9ddfdafa19d2127de63058870e3bba52937c97e3
Paolo Arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-06-01 02:04:01 +0800

    Complete Exercise 2.19: Data for countries, step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,44 +1,48 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-const Search = ({value, handler}) => {
-  return (
-  <>
-    Find Countries: <input value={value} onChange={handler} />
-  </>
-  )
-}
+const Search = ({value, handler}) => (<> Find Countries: <input value={value} onChange={handler} /> </>)
 
-const Result = ({countries, handler}) => {
-  if (countries.length > 10) {
-    return <p>Too many matches. Please specify your search. </p>
-  } else if (countries.length === 1) {
-    return <Country data={countries[0]} handler={handler}/>
+const Result = ({countries, setSelected, selected}) => {
+  if (selected === null) {
+    if (countries.length > 10) {
+      return <p>Too many matches. Please specify your search. </p>
+    } else if (countries.length === 1) {
+      setSelected(countries[0])
+    } else {
+      return countries.map(c => <div key={c.name.common}> {c.name.common} <button onClick={() => setSelected(c)}>Show</button> </div>)
+    }
   } else {
-    return countries.map(c => <div key={c.name.common}> {c.name.common} </div>)
+    return <Country selected={selected} />
   }
 }
 
-const Country = ({data}) => {
-  return (
-    <div>
-      <h1> {data.name.common} </h1>
-      <div>Capital: {data.capital}</div>
-      <div>Area: {data.area}</div>
-      
-      <h1> Languages </h1>
-      <ul>
-        {Object.values(data.languages).map(l => <li key={l}>{l}</li>)}
-      </ul>
-      <img src={data.flags.png} alt={`Flag for ${data.name.common}`} />
-    </div>
-  )
+const Country = ({selected}) => {
+  if (selected !== null) {
+    console.log(selected)
+    return (
+      <div>
+        <h1> {selected.name.common} </h1>
+        <div>Capital: {selected.capital}</div>
+        <div>Area: {selected.area}</div>
+        
+        <h1> Languages </h1>
+        <ul>
+          {Object.values(selected.languages).map(l => <li key={l}>{l}</li>)}
+        </ul>
+        <img src={selected.flags.png} alt={`Flag for ${selected.name.common}`} />
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 
 const App = () => {
   //States & Constants
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
+  const [selected, setSelected] = useState(null)
   const allUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
   const filteredCountries = search !== '' ? countries.filter(c => c.name.common.toLowerCase().includes(search.toLowerCase())) : countries
 
@@ -53,6 +57,7 @@ const App = () => {
   //Handlers
   const handleSearch = (e) => {
     setSearch(e.target.value)
+    setSelected(null)
   }
 
   //Statements
@@ -61,7 +66,7 @@ const App = () => {
   return (
     <>
     <Search value={search} handler={handleSearch} />
-    <Result countries={filteredCountries} />
+    <Result countries={filteredCountries} selected={selected} setSelected={setSelected} />
     </>
   )
 }
