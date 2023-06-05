7d3defc47e02ee29b0fdb641c8cc6d93cc918b3f
paolo-arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-06-05 14:21:06 +0800

    Configure fly.io for deployment

--- part3/notes/src/index.js
+++ part3/notes/src/index.js
@@ -1,7 +1,63 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+morgan.token('body', (req, res) => { JSON.stringify(req.body) })
+const app = express()
 
-import App from './App'
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+app.get('/', (request, response) =>{
+    response.send('<h1>Hello, World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen({PORT}, () => {
+    console.log(`Server running on port ${PORT}`)
+})
