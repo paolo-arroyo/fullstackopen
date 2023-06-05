f3221ae383ecea79aae00049b24a3cbafa479f65
paolo-arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-06-05 14:07:20 +0800

    Copy notes to part3

---
+++ part3/notes/src/components/Note.js
@@ -0,0 +1,11 @@
+const Note = ({note, toggleImportance}) => {
+    const label = note.important ? "Make Not Important" : "Make Important"
+    return (
+        <li className="note">
+            {note.content}
+            <button onClick={toggleImportance}> {label} </button>
+        </li>
+    )
+}
+
+export default Note
